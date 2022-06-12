9e12e027838221032d4c6a3eddb15a5cf1c78685
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-06-13 06:27:38 +0900

    Feat: Translator API

--- api.py
+++ api.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel
 from transformers import RealmRetriever, RealmTokenizer, RealmForOpenQA
 from fastapi import FastAPI
 
-retriever = RealmRetriever.from_pretrained("google/realm-orqa-nq-openqa")
-tokenizer = RealmTokenizer.from_pretrained("google/realm-orqa-nq-openqa")
-model = RealmForOpenQA.from_pretrained("google/realm-orqa-nq-openqa", retriever=retriever)
+# retriever = RealmRetriever.from_pretrained("google/realm-orqa-nq-openqa")
+# tokenizer = RealmTokenizer.from_pretrained("google/realm-orqa-nq-openqa")
+# model = RealmForOpenQA.from_pretrained("google/realm-orqa-nq-openqa", retriever=retriever)
 
 app = FastAPI()
 
@@ -22,11 +22,13 @@ def read_bot():
 @app.post("/")
 def post_bot(user: User):
     question = user.query
-    question_ids = tokenizer([question], return_tensors="pt")
-
-    reader_output, predicted_answer_ids = model(**question_ids, return_dict=False)
-    predicted_answer = tokenizer.decode(predicted_answer_ids)
-    result_dict = {"result": predicted_answer}
+    print(question)
+    # question_ids = tokenizer([question], return_tensors="pt")
+    #
+    # reader_output, predicted_answer_ids = model(**question_ids, return_dict=False)
+    # predicted_answer = tokenizer.decode(predicted_answer_ids)
+    # result_dict = {"result": predicted_answer}
+    result_dict = {"result": question}
     return result_dict
 
 
--- chating_interface/Server.js
+++ chating_interface/Server.js
@@ -3,8 +3,39 @@ const fetch = require('node-fetch');
 
 const express = require("express");
 const http = require('http');
+require('dotenv').config({ path: '../.env' });
+const {TranslationServiceClient} = require('@google-cloud/translate');
+const translationClient = new TranslationServiceClient();
 
+const projectId = 'propane-primacy-353119';
+const location = 'global';
+const text = '안녕하세요!';
 
+async function translateKoToEng(text) {
+    const request = {
+        parent: `projects/${projectId}/locations/${location}`,
+        contents: [text],
+        mimeType: 'text/plain', // mime types: text/plain, text/html
+        sourceLanguageCode: 'ko',
+        targetLanguageCode: 'en',
+    };
+
+    const [response] = await translationClient.translateText(request);
+    return response.translations[0].translatedText
+}
+
+async function translateEngToKo(text) {
+    const request = {
+        parent: `projects/${projectId}/locations/${location}`,
+        contents: [text],
+        mimeType: 'text/plain', // mime types: text/plain, text/html
+        sourceLanguageCode: 'en',
+        targetLanguageCode: 'ko',
+    };
+
+    const [response] = await translationClient.translateText(request);
+    return response.translations[0].translatedText
+}
 
 const PORT = 3737;
 
@@ -35,13 +66,32 @@ socket.sockets.on('connection', function(client){
 
     client.on('serverReceiver', function(value){
         socket.sockets.emit('clientReceiver', {clientID: clientID, message: value});
-        fetch("http://127.0.0.1:8000/", {
-            method: "POST",
-            headers: { "Content-Type": "application/json",},
-            body: JSON.stringify({
-                query: value,
-            }),
-        }).then((response) => response.json()).then((data) =>
-            socket.sockets.emit('clientReceiver', {clientID: 'chatbot', message: JSON.stringify(data['result'])}));
+        translateKoToEng(value)
+            .then(
+                (result) => fetch("http://127.0.0.1:8000/",
+                    {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json",},
+                        body: JSON.stringify({
+                            query: result,
+                        }
+                        ),
+                    }
+                    )
+                    .then(
+                        (response) => response.json()
+                    )
+                    .then(
+                        (data) =>
+                            translateEngToKo(JSON.stringify(data['result'])).then(
+                                (translated_result) =>
+                                    socket.sockets
+                                        .emit('clientReceiver',
+                                            {clientID: 'chatbot', message: translated_result})
+                            )
+
+                    )
+            )
+
     });
 });
