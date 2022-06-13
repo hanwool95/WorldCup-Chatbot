f88357f95848ed4ff173cd890090d664263d570c
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-06-14 05:24:50 +0900

    Fix: duplicated

--- train/openqa.py
+++ train/openqa.py
@@ -13,7 +13,7 @@ num_workers = 5
 cuda_condition = torch.cuda.is_available()
 device = torch.device("cuda:0" if cuda_condition else "cpu")
 
-retriever = RealmRetriever.from_pretrained("google/realm-orqa-nq-openqa").to(device)
+retriever = RealmRetriever.from_pretrained("google/realm-orqa-nq-openqa")
 tokenizer = RealmTokenizer.from_pretrained("google/realm-orqa-nq-openqa")
 model = RealmForOpenQA.from_pretrained("google/realm-orqa-nq-openqa", retriever=retriever).to(device)
 
@@ -22,24 +22,6 @@ query.load_team_data('../crawling/team.csv')
 query.load_player_data('../crawling/player.csv')
 query.load_match_data('../crawling/match.csv')
 query.make_query_answer()
-
-# set optimizer
-param_optim = list(model.named_parameters())
-no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-
-optimizer_grouped_parameters = [{
-            'params': [
-                p for n, p in param_optim
-                if not any(nd in n for nd in no_decay)
-            ],
-            'weight_decay':
-            0.01
-        }, {
-            'params':
-            [p for n, p in param_optim if any(nd in n for nd in no_decay)],
-            'weight_decay':
-            0.0
-        }]
 
 optim = AdamW(model.parameters(), lr=lr)
 torch.cuda.empty_cache()
