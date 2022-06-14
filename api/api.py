2c7aecad4f0aa25f4c25a2f52db2f1b597e121ab
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-06-14 14:52:01 +0900

    Feat: train output in API

--- api/api.py
+++ api/api.py
@@ -5,7 +5,8 @@ from fastapi import FastAPI
 
 retriever = RealmRetriever.from_pretrained("google/realm-orqa-nq-openqa")
 tokenizer = RealmTokenizer.from_pretrained("google/realm-orqa-nq-openqa")
-model = RealmForOpenQA.from_pretrained("google/realm-orqa-nq-openqa", retriever=retriever)
+#model = RealmForOpenQA.from_pretrained("google/realm-orqa-nq-openqa", retriever=retriever)
+model = RealmForOpenQA.from_pretrained("../train/output/1", retriever=retriever)
 
 app = FastAPI()
 
@@ -23,11 +24,14 @@ def read_bot():
 def post_bot(user: User):
     question = user.query
     print(question)
-    question_ids = tokenizer([question], return_tensors="pt")
+    try:
+        question_ids = tokenizer([question], return_tensors="pt")
 
-    reader_output, predicted_answer_ids = model(**question_ids, return_dict=False)
-    predicted_answer = tokenizer.decode(predicted_answer_ids)
-    result_dict = {"result": predicted_answer}
+        reader_output, predicted_answer_ids = model(**question_ids, return_dict=False)
+        predicted_answer = tokenizer.decode(predicted_answer_ids)
+        result_dict = {"result": predicted_answer}
+    except:
+        result_dict = {"result": "Sorry, we couldn't find the answer you were looking for."}
     # result_dict = {"result": question}
     return result_dict
 
